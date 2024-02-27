c582e7be01a5c1f0f77ecab3d58aedbf7b4c26fc
Brian <briangiovanni03@gmail.com>
2024-02-27 09:08:15 +0700

    Final changes

--- part13/script.js
+++ part13/script.js
@@ -1,12 +1,19 @@
 const container = document.querySelector(".container");
-const main = document.querySelector('.main')
+const main = document.querySelector(".main");
+const thumbs = document.querySelectorAll(".thumb");
 
 container.addEventListener("click", (e) => {
   if (e.target.className == "thumb") {
-    main.src = e.target.src
-    main.classList.add('fade')
+    main.src = e.target.src;
+    main.classList.add("fade");
     setTimeout(() => {
-      main.classList.remove('fade')
-    }, 500)
+      main.classList.remove("fade");
+    }, 500);
+
+    thumbs.forEach((thumb) => {
+      thumb.className = "thumb";
+    });
+
+    e.target.classList.add("active");
   }
 });
